ec4a5ef9767892aa6c4387a0aee23f7bd38575ee
zorrowm <zorrowm@126.com>
2024-11-23 12:19:05 +0800

    1

--- 2dol/assets/WidgetUtils-Cybxret4.js
+++
@@ -1,1 +0,0 @@
-import"./index-C-w6kMP4.js";import"./xframelib-exp-Dokv845S.js";function f(n,r){let i;for(const e of n)if(e.path===r){i=e;break}else if(e.children){const t=f(e.children,r);if(t){i=t;break}}return i}export{f};
