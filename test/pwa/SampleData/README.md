70cc609af887ae613a7adbeddeb32207f662e308
zorrowm <zorrowm@126.com>
2026-02-03 15:18:20 +0800

    添加pwa测试网站

---
+++ test/pwa/SampleData/README.md
@@ -0,0 +1,52 @@
+# Quasar App (vue-widget-quasar-clivite-template)
+
+vue widget quasar template project
+
+## 主要技术
+
+vue3
+quasar-cli
+vite
+@opentiny
+widget
+
+### 更改记录
+
+- 202401009
+  升级更新依赖库版本,更新public/cesium；
+  增加dexie库支持本地数据库操作；
+  增加vite-plugin-checker库进行单独线程深度ts、eslint检查;
+  使用VITE_CJS_IGNORE_WARNING=true来忽略该错误"The CJS build of Vite's Node API is deprecated. See https://vitejs.dev/guide/troubleshooting.html#vite-cjs-node-api-deprecated for more details."
+
+- 20240719
+  升级xframelib后，能简化LayoutContainer的初始化
+
+- 20240607
+  增加引入comlink库；
+  给SysConfig.js增加Enables对象，控制部分功能是否启用;
+  引入turf-async异步多线程库；
+  引入CesiumOfflineCache的js库
+
+- 20240605
+  增加PopoverPanel组件；
+  升级依赖库
+  更新public下cesium文件夹文件版本
+- 20240529
+  增加ImageViewer组件；
+  增加vite-plugin-webworker-service插件支持线程；
+  升级依赖库
+
+- 20240221
+  升级依赖库版本;
+  升级@quasar/app-vite，依赖Vite 5版本;
+  使用Prettier配置格式化代码
+  修改相关配置
+- 20231220
+  引入workerpool库和修改相关配置；
+  解决打包时tsx组件报错问题
+- 20231219
+  更新依赖库版本；
+  调整部分组件位置
+
+- 20230416
+  初始版本：解决依赖 vite 插件和 build 问题
---
+++ test/pwa/SysConfig.js
@@ -0,0 +1,134 @@
+/**
+ * 通用的 V1.4,针对0.6.1以上版本
+ * 2025-4-25 规范与补充，针对0.9.5匹配，增加OnlyUserVerify
+ */
+const SysConfig = {
+  //#region ********系统界面相关配置
+  UI: {
+    /*必须，系统配置标题，必须 */
+    SiteTitle: '基于Widget Quasar的开发模板',
+    CopyRight: 'Copyright ©XXXX  2022-2026',
+    WebSite: 'http://www.XXXX.com',
+    /**是否是能访问互联网，还是内网部署应用*/
+    IsInternet: true,
+    LockTime: 3600, //1小时
+    IsNoLogin: false, //无需登录页面,true时不登录
+    OnlyUserVerify: false, //只用户验证，不验证角色与功能授权
+    GrayMode: false, //是否启用网站暗灰模式，悼念日，默认为false
+    ProductLog: false, //是否在产品发布后输出开发日志记录
+    // Theme:'dark'//系统的主题
+  },
+  //#endregion
+
+  //#region ********后台服务地址配置
+  ServiceURL: {
+    /**
+     * 用户登录（统一用户登录）（不能带"/"）
+     * http://192.168.1.12:83
+     */
+    LoginAuthURL: 'https://auth.gis.digsur.com',
+    /**
+     * 图标在线服务地址（不能带"/"）
+     * https://gis-icon.digsur.com/online
+     */
+    IconServiceURL: 'https://icon.gis.digsur.com/online',
+    /**
+     * 文件管理服务地址（统一文件管理：后台）
+     */
+    FileServiceURL: '', //文件管理
+    /**
+     * 文件管理（统一文件管理：前台）
+     */
+    FileOnlineURL: '', //文件管理，在线
+    /**
+     * 在线日志服务
+     */
+    LogServiceURL: '', //日志服务
+    /**
+     * Axios普通WebAPI的BaseURL
+     * 全局默认的http请求地址（一般与主hprose相同或不同）;文件上传地址
+     */
+    DefaultWebAPI: 'https://hunanviewmulti.gis.digsur.com',
+    /**
+     * 默认HproseAPI的服务地址
+     */
+    DefaultHproseAPI: 'http://192.168.1.120:5139/hprose', //https://gis-image.digsur.com/ImageAdmin
+    //数据集管理http://192.168.1.33:1001/DataSource
+    DatasetHproseAPI: 'https://gis-image.digsur.com/DataSource',
+    // DatasetHproseAPI: 'http://192.168.1.47:1001/DataSource',
+
+    //渲染样式色带图片
+    DefaultStyleColorImage: 'https://gis-image.digsur.com',
+    //后台瓦片服务地址
+    WMTSService: 'https://gis-image.digsur.com/IMGWMTS',
+    /**
+     * 二维地图默认地址
+     */
+    Map2dStyleURL:'https://msmg-main.gis.digsur.com/api/Vtpk/GetStyleJson?layerId=38d43f204c4542baa0a8a6162bc698be',
+    MultiSourceURL: "http://hunanviewmulti.gis.digsur.com"
+  },
+  //#endregion
+
+  //#region ********地图配置（天地图KEY）
+  /**
+   * 地图Key,值为string或array
+   */
+  MapKeys: {
+    /**
+     * 天地图服务的授权Key
+     */
+    TDTKey: [
+      'f9b51cc0282fa69451df24be416107b6',
+      'a90b856f2ade4b97f683cbc9c3c2702e',
+      '3566b0e50b26951da109cfea07e583c4',
+      '4ed383c68a325d012da2f0195b9609fe',
+    ],
+    /**
+     * MapboxKey
+     * pk.eyJ1IjoiemdlbyIsImEiOiJja3E2MGE2NmIxbm45Mm5vNHpnOWZlZ3BlIn0.lDaMkti77XPv0_so0J9apQ
+     */
+    MapboxKey:
+      'pk.eyJ1IjoiY2hyaXNuaW5nIiwiYSI6ImNrZzk3dmNveTA2cGUycXAyNXJ3bWNsOHMifQ._4oFj3iqj5yWWvbuONDYnw',
+    /**
+     * Cesium Key
+     */
+    CesiumKey:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIzZjdkOGRkYi1hYzIxLTQ4MDMtYjZiMC0zODg5YjI2ZTRlZjIiLCJpZCI6MjgyLCJzY29wZXMiOlsiYXNsIiwiYXNyIiwiYXN3IiwiZ2MiXSwiaWF0IjoxNTYyMDEyNTIyfQ.aVsGtowVeK_5C25G5-WCK7bZHyfXUl_zQ5Ud7TKsq0U',
+    /**
+     * Google地图Key
+     */
+    // GoogleKe?: string;
+  },
+  //#endregion
+
+  //#region ******** API服务路径
+  APIPath: {
+    SignalR: '/chathub',
+  },
+  //#endregion
+
+  //#region **********用于控制功能是否启用
+  Enables: {
+    CesiumOfflineCache: true, //Cesium缓存
+  },
+  //#endregion
+};
+
+//#region *********其他可扩展执行的JS代码
+globalThis.SysConfig=SysConfig;
+//默认设置网站Title
+document.title = SysConfig.UI.SiteTitle;
+//设置样式
+var htmlRoot = document.getElementById('htmlRoot');
+//颜色变灰
+if (SysConfig.UI.GrayMode) htmlRoot.className = 'grayMode';
+//#endregion
+//设置主题样式
+if (SysConfig.UI.Theme) {
+  const link = document.createElement('link');
+  link.type = 'text/css';
+  link.rel = 'stylesheet';
+  link.href = './theme/' + SysConfig.UI.Theme + '.css';
+  const head = document.getElementsByTagName('head')[0];
+  head.appendChild(link);
+}
